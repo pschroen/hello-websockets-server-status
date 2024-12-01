9956f6d14b3f2af51b78d7a000642c84f74039b2
Patrick Schroen <ps@ufotechnologies.com>
2024-12-01 11:12:48 -0500

    Use seconds instead of milliseconds

--- server.js
+++ server.js
@@ -47,11 +47,11 @@ async function getStatus() {
 		return;
 	}
 
-	const currentTime = Date.now();
+	const currentTime = Math.floor(Date.now() / 1000); // seconds
 
 	try {
 		serverUptime = (await exec('cat /proc/uptime')).stdout;
-		serverUptime = Number(serverUptime.split(' ')[0]) * 1000; // milliseconds
+		serverUptime = Number(serverUptime.split(' ')[0]);
 	} catch (err) {
 		console.warn(err.stderr);
 	}
